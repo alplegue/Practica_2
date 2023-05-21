bd854a84e6ff262202d99d6d811ece3f7c5b730c
alplegue <124245499+alplegue@users.noreply.github.com>
2023-05-21 02:27:14 +0200

    Update Practica2_2PRPA.py

--- Practica2_2PRPA.py
+++ Practica2_2PRPA.py
@@ -2,337 +2,154 @@
 """
 Álvaro Pleguezuelos Escobar
 
-A diferencia con el código anterior, este permite el paso de peatones en ambas direcciones en
-el puente con la restricción de que no pueden haber peatones y coches a la vez.
-Además vamos a establecer un orden de entrada al puente.
+A diferencia con el código anterior, este permite el paso de peatones en ambas direcciones en el puente con la restricción de que no pueden haber peatones y coches a la vez.
+Además, vamos a establecer un orden de entrada al puente.
 """
 import time
-
 import random
-
 from multiprocessing import Lock, Value, Condition, Process, Manager
 
-
+# Constantes para las direcciones
 SOUTH = 'SOUTH'
-
 NORTH = 'NORTH'
 
-NCARS = 10 #Numero de coches
+# Número de coches y peatones
+NCARS = 10  # Número de coches
+NPED = 10  # Número de peatones
 
-NPED = 10 #Numero de peatones
-
-TIME_CARS_NORTH = 0.5  # a new car enters each 0.5s
-
-TIME_CARS_SOUTH = 0.5  # a new car enters each 0.5s
-
-TIME_PED = 3 # a new peaton enters each 5s
+# Tiempos de espera
+TIME_CARS_NORTH = 0.5  # Un nuevo coche entra cada 0.5 segundos
+TIME_CARS_SOUTH = 0.5  # Un nuevo coche entra cada 0.5 segundos
+TIME_PED = 3  # Un nuevo peatón entra cada 3 segundos
 
 class Monitor():
-    
     def __init__(self, manager):
-        
-        self.mutex = Lock()
-        
-        self.peatones = Value('i', 0)
-        
-        self.cochesnorte = Value('i', 0) 
-        
-        self.cochessur = Value('i', 0)
-        
-        self.entradapeatones = Condition(self.mutex) 
-        
-        self.entradacoches_norte = Condition(self.mutex) 
-        
-        self.entradacoches_sur = Condition(self.mutex) 
-        
-        self.esperacoches_norte = Value('i', 0) 
-        
-        self.esperacoches_sur = Value('i', 0)
-        
-        self.esperapeatones = Value('i', 0)
-        
-        self.turn = Value('i', 0)
-        
-        # El 0  indica puente vacío
-        
-        # El 1 indica coches dirección norte cruzando el puente
-        
-        # El 2 indica coches dirección sur cruzando el puente
-        
-        # El 3 indica que hay peatones cruzando el puente
+        self.mutex = Lock()  # Mutex para garantizar exclusión mutua
+        self.peatones = Value('i', 0)  # Contador de peatones en el puente
+        self.cochesnorte = Value('i', 0)  # Contador de coches en dirección norte en el puente
+        self.cochessur = Value('i', 0)  # Contador de coches en dirección sur en el puente
+        self.entradapeatones = Condition(self.mutex)  # Condición de entrada para peatones
+        self.entradacoches_norte = Condition(self.mutex)  # Condición de entrada para coches en dirección norte
+        self.entradacoches_sur = Condition(self.mutex)  # Condición de entrada para coches en dirección sur
+        self.esperacoches_norte = Value('i', 0)  # Contador de coches en dirección norte esperando
+        self.esperacoches_sur = Value('i', 0)  # Contador de coches en dirección sur esperando
+        self.esperapeatones = Value('i', 0)  # Contador de peatones esperando
+        self.turn = Value('i', 0)  # Indica el estado actual del puente:
+        # 0 indica puente vacío
+        # 1 indica coches dirección norte cruzando el puente
+        # 2 indica coches dirección sur cruzando el puente
+        # 3 indica que hay peatones cruzando el puente
 
     def permisocoche_norte(self):
-        
-        condicion = (self.cochessur.value == 0 and self.peatones.value == 0) and (self.turn.value == 1)\
-            or self.turn.value == 0
-            
+        # Verifica si hay permiso para que un coche en dirección norte entre al puente
+        # No pueden haber peatones y coches en el puente al mismo tiempo
+        # Si es el turno de los coches en dirección norte o el puente está vacío, devuelve True
+        condicion = (self.cochessur.value == 0 and self.peatones.value == 0) and (self.turn.value == 1) or self.turn.value == 0
         return condicion
-    
+
     def permisocoche_sur(self):
-        
-        condicion = (self.cochesnorte.value == 0 and self.peatones.value == 0) and (self.turn.value == 2)\
-            or self.turn.value == 0
-            
+        # Verifica si hay permiso para que un coche en dirección sur entre al puente
+        # No pueden haber peatones y coches en el puente al mismo tiempo
+        # Si es el turno de los coches en dirección sur o el puente está vacío, devuelve True
+        condicion = (self.cochesnorte.value == 0 and self.peatones.value == 0) and (self.turn.value == 2) or self.turn.value == 0
         return condicion
-        
+
     def solicitaentrar_coche(self, direccion):
+        # Un coche solicita entrar al puente en una dirección específica
         self.mutex.acquire()
-       
-        if (direccion == NORTH):
-            
-            self.esperacoches_norte.value += 1
-            
-            self.entradacoches_norte.wait_for(self.permisocoche_norte)
-            
-            self.esperacoches_norte.value -= 1
-            
-            self.cochesnorte.value += 1 
-            
-            self.turn.value = 1 
-            
-            
-        if (direccion == SOUTH):
-            
-            self.esperacoches_sur.value += 1
-            
-            self.entradacoches_sur.wait_for(self.permisocoche_sur)
-            
-            self.esperacoches_sur.value -= 1
-            
-            self.cochessur.value += 1
-            
-            self.turn.value = 2 
-            
-            
+        if direccion == NORTH:
+            self.esperacoches_norte.value += 1  # Incrementa el contador de coches en dirección norte esperando
+            self.entradacoches_norte.wait_for(self.permisocoche_norte)  # Espera hasta que se cumpla la condición para entrar
+            self.esperacoches_norte.value -= 1  # Decrementa el contador de coches en dirección norte esperando
+            self.cochesnorte.value += 1  # Incrementa el contador de coches en dirección norte en el puente
+            self.turn.value = 1  # Establece el turno de los coches en dirección norte
+        if direccion == SOUTH:
+            self.esperacoches_sur.value += 1  # Incrementa el contador de coches en dirección sur esperando
+            self.entradacoches_sur.wait_for(self.permisocoche_sur)  # Espera hasta que se cumpla la condición para entrar
+            self.esperacoches_sur.value -= 1  # Decrementa el contador de coches en dirección sur esperando
+            self.cochessur.value += 1  # Incrementa el contador de coches en dirección sur en el puente
+            self.turn.value = 2  # Establece el turno de los coches en dirección sur
         self.mutex.release()
 
     def salidacoche(self, direccion):
-        
+        # Un coche sale del puente en una dirección específica
         self.mutex.acquire()
-        
-        if (direccion == NORTH):
-            
-            self.cochesnorte.value -= 1
-            
-            if self.esperacoches_sur.value != 0:
-                
-                self.turn.value = 2
-                
-            elif self.esperapeatones.value != 0 :
-                
-                self.turn.value = 3
-                
-            else:
-                
-                self.turn.value = 0
-                
-            if self.cochesnorte.value == 0:
-                
-                self.entradacoches_sur.notify_all()
-                
-                self.entradapeatones.notify_all()
-
-        if (direccion == SOUTH):
-            
-            self.cochessur.value -= 1
-            
-            if self.esperacoches_norte.value != 0 :
-                
-                self.turn.value = 1 
-                
-            elif self.esperapeatones.value != 0 :
-                
-                self.turn.value = 3 
-                
-            else:
-                
-                self.turn.value = 0
-                
-                
-            if self.cochessur.value == 0:
-                
-                self.entradacoches_norte.notify_all()
-                
-                self.entradapeatones.notify_all()
-                
-    
+        if direccion == NORTH:
+            self.cochesnorte.value -= 1  # Decrementa el contador de coches en dirección norte en el puente
+            if self.esperacoches_sur.value != 0:  # Si hay coches en dirección sur esperando
+                self.turn.value = 2  # Establece el turno de los coches en dirección sur
+        if direccion == SOUTH:
+            self.cochessur.value -= 1  # Decrementa el contador de coches en dirección sur en el puente
+            if self.esperacoches_norte.value != 0:  # Si hay coches en dirección norte esperando
+                self.turn.value = 1  # Establece el turno de los coches en dirección norte
         self.mutex.release()
-        
-    def permisopeaton(self):
-        
-        condicion = (self.cochessur.value == 0 and self.cochesnorte.value == 0) and (self.turn.value == 3)\
-            or self.turn.value == 0
-       
-        return condicion
-    
+
     def solicitaentrar_peaton(self):
-        
+        # Un peatón solicita entrar al puente
         self.mutex.acquire()
-        
-        self.esperapeatones.value += 1
-        
-        self.entradapeatones.wait_for(self.permisopeaton)
-        
-        self.esperapeatones.value -= 1
-        
-        self.peatones.value += 1
-        
-        self.turn.value = 3 
-        
+        self.esperapeatones.value += 1  # Incrementa el contador de peatones esperando
+        self.entradapeatones.wait_for(lambda: self.peatones.value == 0)  # Espera hasta que no haya peatones en el puente
+        self.esperapeatones.value -= 1  # Decrementa el contador de peatones esperando
+        self.peatones.value += 1  # Incrementa el contador de peatones en el puente
+        self.turn.value = 3  # Establece el turno de los peatones
         self.mutex.release()
 
     def salidapeaton(self):
-        
+        # Un peatón sale del puente
         self.mutex.acquire()
-        
-        self.peatones.value -= 1
-        
-        if self.esperacoches_sur.value != 0 :
-            
-            self.turn.value = 2 
-            
-        elif self.esperacoches_norte.value != 0 : 
-            
-            self.turn.value = 1
-            
+        self.peatones.value -= 1  # Decrementa el contador de peatones en el puente
+        if self.esperapeatones.value != 0:  # Si hay peatones esperando
+            self.turn.value = 3  # Establece el turno de los peatones
+        elif self.esperacoches_norte.value != 0:  # Si hay coches en dirección norte esperando
+            self.turn.value = 1  # Establece el turno de los coches en dirección norte
+        elif self.esperacoches_sur.value != 0:  # Si hay coches en dirección sur esperando
+            self.turn.value = 2  # Establece el turno de los coches en dirección sur
         else:
-            
-            self.turn.value = 0
-
-        if self.peatones.value == 0:
-            
-            self.entradacoches_norte.notify_all()
-            
-            self.entradacoches_sur.notify_all()
-            
+            self.turn.value = 0  # El puente está vacío
         self.mutex.release()
-    
-    def __repr__(self) -> str:
-        
-        return f'Monitor< Coches_Norte: {self.cochesnorte.value}, Coches_Norte_Waiting : {self.esperacoches_norte.value},\
-            Coches_Sur: {self.cochessur.value}, Coches_Sur_Waiting : {self.esperacoches_sur.value},\
-                Peatones :  {self.peatones.value}, Peatones_Waiting : {self.esperapeatones.value},> \n'
-    
-    
 
-def retrasoCochenorte(factor = TIME_CARS_NORTH) -> None:
-    
-    time.sleep(factor)
 
-def retrasoCochesur(factor = TIME_CARS_SOUTH) -> None:
-    
-    time.sleep(factor)
+def coche(direccion, monitor):
+    # Función para simular el comportamiento de un coche
+    time.sleep(random.uniform(0, 1))  # Tiempo de espera antes de solicitar entrar al puente
+    monitor.solicitaentrar_coche(direccion)  # Solicita entrar al puente en la dirección especificada
+    print(f'Coche en dirección {direccion} entra al puente')
+    time.sleep(1)  # Tiempo que tarda el coche en cruzar el puente
+    monitor.salidacoche(direccion)  # Sale del puente en la dirección especificada
+    print(f'Coche en dirección {direccion} sale del puente')
 
-def retrasopeaton(factor = TIME_PED) -> None:
-    
-    time.sleep(factor)
 
-def coche(cid: int, direccion: int, monitor: Monitor)  -> None:
-    
-    print(f"Coche {cid} con dirección {direccion} quiere entrar al puente.-- {monitor} --")
-    
-    monitor.solicitaentrar_coche(direccion)
-    
-    print(f"Coche {cid} con dirección {direccion} ha entrado en el puente.-- {monitor} --")
-    
-    if direccion==NORTH :
-        
-        retrasoCochenorte()
-        
-    else:
-        
-        retrasoCochesur()
-        
-    print(f"Coche {cid} con dirección {direccion} abandonando el puente.-- {monitor} --")
-    
-    monitor.salidacoche(direccion)
-    
-    print(f"Coche {cid} con dirección {direccion} ha salido del puente.-- {monitor} --")
-    
-def peaton(pid: int, monitor: Monitor) -> None:
-    
-    print(f"Peaton {pid} quiere entrar al puente.-- {monitor} --")
-    
-    monitor.solicitaentrar_peaton()
-    
-    print(f"Peaton {pid} ha entrado en el puente.-- {monitor} --")
-    
-    retrasopeaton()
-    
-    print(f"Peaton {pid} abandonado el puente.-- {monitor} --")
-    
-    monitor.salidapeaton()
-    
-    print(f"Peaton {pid} ha salido del puente.-- {monitor} --")
-    
+def peaton(monitor):
+    # Función para simular el comportamiento de un peatón
+    time.sleep(random.uniform(0, 1))  # Tiempo de espera antes de solicitar entrar al puente
+    monitor.solicitaentrar_peaton()  # Solicita entrar al puente
+    print('Peatón entra al puente')
+    time.sleep(1)  # Tiempo que tarda el peatón en cruzar el puente
+    monitor.salidapeaton()  # Sale del puente
+    print('Peatón sale del puente')
 
-def gen_peaton(monitor: Monitor) -> None:
-    
-    pid = 0
-    
-    plst = []
-    
-    for _ in range(NPED):
-        
-        pid += 1
-        
-        p = Process(target=peaton, args=(pid, monitor))
-        
-        p.start()
-        
-        plst.append(p)
-        
-        time.sleep(random.expovariate(1/TIME_PED))
-
-    for p in plst:
-        
-        p.join()
-        
-def gen_coches(direccion: int, time_cars, monitor: Monitor) -> None:
-    
-    cid = 0
-    
-    plst = []
-    
-    for _ in range(NCARS):
-        
-        cid += 1
-        
-        p = Process(target=coche, args=(cid, direccion, monitor))
-        
-        p.start()
-        
-        plst.append(p)
-        
-        time.sleep(random.expovariate(1/time_cars))
-
-    for p in plst:
-        
-        p.join()
-
-def main():
-    
-    manager = Manager()
-    
-    monitor = Monitor(manager)
-    
-    coches_norte = Process(target=gen_coches, args=(NORTH, TIME_CARS_NORTH, monitor))
-    
-    coches_sur = Process(target=gen_coches, args=(SOUTH, TIME_CARS_SOUTH, monitor))
-    
-    peat = Process(target=gen_peaton, args=(monitor,))
-    
-    coches_norte.start()
-    
-    coches_sur.start()
-    
-    peat.start()
-    
-    coches_norte.join()
-    
-    coches_sur.join()
-    
-    peat.join()
 
 if __name__ == '__main__':
-    main()
+    manager = Manager()
+    monitor = Monitor(manager)
+
+    processes = []
+
+    # Crear procesos para los coches
+    for i in range(NCARS):
+        direction = SOUTH if random.random() < 0.5 else NORTH  # Determina la dirección del coche de forma aleatoria
+        p = Process(target=coche, args=(direction, monitor))
+        processes.append(p)
+
+    # Crear procesos para los peatones
+    for i in range(NPED):
+        p = Process(target=peaton, args=(monitor,))
+        processes.append(p)
+
+    # Iniciar los procesos
+    for p in processes:
+        p.start()
+
+    # Esperar a que todos los procesos terminen
+    for p in processes:
+        p.join()
